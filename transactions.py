a86bc22c0304559df9de06f3994b868632987067
Tania Batista <tanbatista@gmail.com>
2022-05-01 22:41:35 -0400

    add total balances to each transaction

--- transactions.py
+++ transactions.py
@@ -2,4 +2,29 @@ import requests
 
 response = requests.get("https://shakepay.github.io/programming-exercise/web/transaction_history.json")
 data = response.json()
-data = sorted(data, key=lambda d: d['createdAt'])
+transactions = sorted(data, key=lambda d: d['createdAt'])
+
+balance = {
+    "CAD": 0,
+    "BTC": 0,
+    "ETH": 0
+}
+
+for txn in transactions:
+    currency = txn['currency']
+    amount = txn['amount']
+
+    if txn['direction'] == 'debit':
+        balance[currency] -= amount
+
+    if txn['direction'] == 'credit':
+        balance[currency] += amount
+
+    if txn['direction'] is None:
+        balance[currency] -= amount
+
+        to_currency = txn['to']['currency']
+        to_amount = txn['to']['amount']
+        balance[to_currency] += to_amount
+
+    txn['balance'] = balance
