450a08ce3445abf2067bd26dd3fce42baff59e7d
Tania Batista <tanbatista@gmail.com>
2022-05-05 19:18:09 -0400

    Update rates code

--- rates.py
+++ rates.py
@@ -1,7 +1,9 @@
 import requests as requests
 
+
 btc_rates = requests.get("https://shakepay.github.io/programming-exercise/web/rates_CAD_BTC.json").json()
 eth_rates = requests.get("https://shakepay.github.io/programming-exercise/web/rates_CAD_ETH.json").json()
+
 
 rates = {
     'ETH': {rate['createdAt'][:10]: rate['midMarketRate'] for rate in eth_rates},
@@ -9,24 +11,29 @@ rates = {
 }
 
 
+btc_dates = list(rates['BTC'].keys())
+eth_dates = list(rates['ETH'].keys())
+
+
 def get_rate(currency, date):
 
     try:
         return rates[currency][date]
 
+    # if rate doesn't exist for a transaction's date, then return the next rate
     except KeyError:
-        entries = 0
-        total = 0
-        for k, v in rates[currency].items():
-            if k.startswith(date[:9]):
-                entries += 1
-                total += float(v)
-                return total/entries
-            elif k.startswith(date[:7]):
-                entries += 1
-                total += float(v)
-                return total/entries
-            elif k.startswith(date[:5]):
-                entries += 1
-                total += float(v)
-                return total/entries
+
+        if currency == 'BTC':
+            lst = list(rates['BTC'].keys())
+            lst.append(date)
+            tmr_index = sorted(lst).index(date) + 1
+            date = btc_dates[tmr_index]
+            return rates['BTC'][date]
+
+        if currency == 'ETH':
+            lst = list(rates['ETH'].keys())
+            lst.append(date)
+            srted_lst = sorted(lst)
+            tmr_index = srted_lst.index(date) + 1
+            date = eth_dates[tmr_index]
+            return rates['ETH'][date]
